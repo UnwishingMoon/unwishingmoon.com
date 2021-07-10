3e38e36dfa2afe4cef4891127d523f412d16e222
UnwishingMoon <11472973+UnwishingMoon@users.noreply.github.com>
2021-07-10 14:05:37 +0200

    Added bootstrap and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaGithub, FaTwitch, FaTwitter } from 'react-icons/fa';
+import { Col, Container, Row, Image } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container className="d-flex flex-column text-center justify-content-center align-items-center" style={{minHeight: "100vh", marginTop: -5}}>
+            <Row>
+                <Col>
+                    <Image src="./logo.png" rounded fluid style={{width: "50%", maxWidth: 300}} />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h1 className="h2 mt-2 mb-2">UNWISHINGMOON</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <span className="mb-2"><q>Be the change that you wish to see in the world.</q> &mdash; Mahatma Gandhi</span>
+                </Col>
+            </Row>
+            <Row className="mt-2 h5">
+                <Col>
+                    <a href="https://www.twitch.tv/unwishingmoon" target="_blank" rel="noreferrer" title="Twitch">
+                        <FaTwitch color="white" />
+                    </a>
+                </Col>
+                <Col>
+                    <a href="https://github.com/UnwishingMoon" target="_blank" rel="noreferrer" title="Github">
+                        <FaGithub color="white" />
+                    </a>
+                </Col>
+                <Col>
+                    <a href="https://www.twitter.com/_diegocastagna" target="_blank" rel="noreferrer" title="Twitter">
+                        <FaTwitter color="white" />
+                    </a>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
